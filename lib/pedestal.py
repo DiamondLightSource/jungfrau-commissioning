4638feb618151e41b148ae6a8071ee1040614833
Graeme Winter <graeme.winter@gmail.com>
2023-06-28 15:32:58 +0100

    Variance

--- lib/pedestal.py
+++ lib/pedestal.py
@@ -3,13 +3,13 @@ import os
 import numpy
 import h5py
 
+gain_keys = {"G0": 0, "G1": 1, "G2": 3}
+
 
 def pedestal_raw_data(directory):
     """Read pedestal data from the files in the named directory"""
 
     result = {}
-
-    gain_keys = {"G0": 0, "G1": 1, "G2": 3}
 
     for module in 0, 1:
         for gain in "G0", "G1", "G2":
@@ -17,4 +17,33 @@ def pedestal_raw_data(directory):
             assert os.path.exists(filename)
             with h5py.File(filename, "r") as f:
                 result[(gain, module)] = f["data"][()]
+    return result
+
+
+def pedestals(directory):
+    """Read pedestal data; filter and average"""
+
+    result = {}
+
+    raw_data = pedestal_raw_data(directory)
+    for module in 0, 1:
+        for gain in "G0", "G1", "G2":
+            key = gain_keys[gain]
+            data = raw_data[(gain, module)]
+            # mask, sum image, sum square image
+            m = numpy.zeros(data.shape[1:], dtype=numpy.int64)
+            i = numpy.zeros(data.shape[1:], dtype=numpy.int64)
+            s = numpy.zeros(data.shape[1:], dtype=numpy.int64)
+            for j in range(data.shape[0]):
+                b = numpy.right_shift(data[j], 14) == key
+                x = data[j] * b
+                m += b
+                i += x
+                s += numpy.square(x)
+            m[m == 0] = 1
+            i = i.astype(numpy.float64) / m
+            s = s.astype(numpy.float64) / m
+            v = s - numpy.square(i)
+            result[(gain, module)] = (i, v)
+
     return result
