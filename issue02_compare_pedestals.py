2edf683f72df4a3ae375c71e6761fb219995c87c
Graeme Winter <graeme.winter@gmail.com>
2023-07-03 14:00:49 +0100

    Side-by-side

--- issue02_compare_pedestals.py
+++ issue02_compare_pedestals.py
@@ -13,6 +13,7 @@ pref = pedestals(d0)
 for module in 0, 1:
     for gain in "G0", "G1", "G2":
         diff = pmi[(gain, module)][0] - pref[(gain, module)][0]
-        pyplot.imshow(diff, vmin=-100, vmax=100)
-        pyplot.colorbar()
+        fig, axes = pyplot.subplots(2)
+        axes[0].imshow(pmi[(gain, module)][0])
+        axes[1].imshow(pref[(gain, module)][0])
         pyplot.show()
